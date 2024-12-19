cfab46f92908b608157e4446cfbabecfe0f2de5c
marbel <othmane.xx90@gmail.com>
2024-12-19 13:56:50 +0100

    http urls

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"log"
 )
 
+const issueURL = "https://api.boot.dev/v1/courses_rest_api/learn-http/issues"
+
 func main() {
-	issues, err := getIssueData()
+	issues, err := getIssueData(issueURL)
 	if err != nil {
 		log.Fatalf("error getting issue data: %v", err)
 	}
-	s := string(issues)
-	fmt.Println(s)
+	prettyData, err := prettify(string(issues))
+	if err != nil {
+		log.Fatalf("error prettifying data: %v", err)
+	}
+	fmt.Println(prettyData)
 }
 
