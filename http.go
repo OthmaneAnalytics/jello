106f2a9c1b05c8dbc9a356674f1cecbde5e8b314
marbel <othmane.xx90@gmail.com>
2024-12-19 18:31:40 +0100

    unmarshal jason

--- http.go
+++ http.go
@@ -26,6 +26,24 @@ func getIssues(url string) ([]Issue, error) {
 		return nil, fmt.Errorf("error creating request: %w", err)
 	}
 	defer res.Body.Close()
+	data, err1 := io.ReadAll(res.Body)
+	var issues []Issue
+	if err1 != nil {
+		return nil, fmt.Errorf("error creading data: %w", err1)
+	}
+	if err = json.Unmarshal(data, &issues); err != nil {
+		return nil, err
+	}
+	return issues, nil
+}
+
+
+func getIssues2(url string) ([]Issue, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
 
 	var issues []Issue
 	decoder := json.NewDecoder(res.Body)
