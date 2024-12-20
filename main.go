15a0511ccbe8e7185a93208b32e315ca31e9cf88
marbel <othmane.xx90@gmail.com>
2024-12-20 08:52:31 +0100

    sections of a url

--- main.go
+++ main.go
@@ -16,6 +16,28 @@ func getDomainNameFromURL(rawURL string) (string, error) {
 
 }
 
+func newParsedURL(urlString string) ParsedURL {
+	parsedUrl, err := url.Parse(urlString)
+	if err != nil {
+		return ParsedURL{}
+	}
+	pass, ok := parsedUrl.User.Password()
+	if !ok {
+		pass = ""
+	}
+	return ParsedURL{
+		protocol: parsedUrl.Scheme,
+		username: parsedUrl.User.Username(),
+		password: pass,
+		hostname: parsedUrl.Hostname(),
+		port:     parsedUrl.Port(),
+		pathname: parsedUrl.Path,
+		search:   parsedUrl.RawQuery,
+		hash:     parsedUrl.Fragment,
+	}
+}
+
+
 func marshalAll[T any](items []T) ([][]byte, error) {
 	var jbytes [][]byte
 	for _, item := range items {
